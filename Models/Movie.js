971a099c7eb488f73e4db53c289a45125101d576
aviraj45 <161598772+aviraj45@users.noreply.github.com>
2024-05-17 14:07:12 +0530

    Add files via upload

---
+++ Models/Movie.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema({
+    // Fields of schema
+    title:{
+        type: String,
+        required: true,
+
+    },
+
+    description: {
+        type: String,
+        required: true,
+    },
+
+    actors: [{type: String, required: true}],
+
+
+
+    releseDate: {
+        type: Date,
+        required: true,
+
+    },
+
+    
+    posterUrl: {
+        type: String,
+        required: true,
+    },
+
+    featured: {
+        type: Boolean,
+    },
+
+    bookings : [ {type: String} ],
+
+    admin: {
+        type: String,
+        required: true,
+    },
+} );
+
+export default mongoose.model("Movie",movieSchema);
